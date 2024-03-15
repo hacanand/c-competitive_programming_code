924ade60beac7b66e6bcd76f0efe4ca418547899
anand <anandpromax@gmail.com>
2024-03-15 23:03:12 +0530

    Refactor code for improved performance

--- B_Array_Fix.cpp
+++ B_Array_Fix.cpp
@@ -1,27 +1,16 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-#define ll long long
-#define loop(n) for (long long i = 0; i < n; i++)
-#define rloop(n) for (long long i = n - 1; i >= 0; i--)
-#define fori(n) for (auto &i : n)
-  
+int t, n, a[100010];
+bool f;
 int main()
 {
-
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output1.txt", "w", stdout);
-#endif
-
-    int t;
     cin >> t;
     while (t--)
     {
-        int n;
         cin >> n;
-        int f = 1;
-        vector<int>a(n);
+        f = 1;
+        for (int i = 1; i <= n; i++)
+            cin >> a[i];
         for (int i = n - 1; i > 0 && f; i--)
             if (a[i] > a[i + 1])
                 if (a[i] / 10 > a[i] % 10 || a[i] % 10 > a[i + 1])
