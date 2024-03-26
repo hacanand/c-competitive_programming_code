0626b6e3526eddbe9a0a9cbe5719a569c2b32689
anand <anandpromax@gmail.com>
2024-03-26 13:33:54 +0530

    Fix array sorting issue in B_Array_Fix.cpp

--- B_Array_Fix.cpp
+++ B_Array_Fix.cpp
@@ -12,11 +12,13 @@ int main()
         for (int i = 1; i <= n; i++)
             cin >> a[i];
         for (int i = n - 1; i > 0 && f; i--)
+
             if (a[i] > a[i + 1])
                 if (a[i] / 10 > a[i] % 10 || a[i] % 10 > a[i + 1])
                     f = 0;
                 else
                     a[i] /= 10;
+
         if (f)
             cout << "YES\n";
         else
