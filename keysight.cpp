b915d1af4d6c917ebce886b69dc1e5de66024c0c
anand <anandpromax@gmail.com>
2024-08-22 18:42:10 +0530

    Refactor code to split array into equal sums

---
+++ keysight.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+int maxSubArraySum(  vector<int> &nums)
+{
+    int maxSoFar = INT_MIN;
+    int maxEndingHere = 0;
+
+    for (int num : nums)
+    {
+        maxEndingHere = max(0, maxEndingHere + num);
+        maxSoFar = max(maxSoFar, maxEndingHere);
+    }
+
+    return maxSoFar;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n;i++){
+        cin >> nums[i];
+    }
+        int maxSum = maxSubArraySum(nums);
+
+    cout  << maxSum << endl;
+
+    return 0;
+}
