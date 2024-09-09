427cd571b7259943b98d118129751f4d530b8c33
anand <anandpromax@gmail.com>
2024-09-09 11:18:54 +0530

    chore: Refactor code to add accenture-oa-square.cpp solution

---
+++ accenture-oa-square.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> v;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    for(auto i:v){
+        double sq = sqrt(i);
+        if(sq*(int)sq==i)
+        {
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
