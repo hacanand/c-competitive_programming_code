1fdee60b50375d35fb5a6f4332ac78cdb03d1360
anand <anandpromax@gmail.com>
2024-09-14 23:37:37 +0530

    Add solution for Accenture OA hour problem to count pairs of minutes

---
+++ accenture-oa-hour.cpp
@@ -0,0 +1,41 @@
+ 
+    #include <iostream>
+    #include <vector>
+    #include <unordered_map>
+    using namespace std;
+
+    int countPairs(vector<int> &minutes)
+    {
+        unordered_map<int, int> remainderCount;
+        int count = 0;
+
+        for (int minute : minutes)
+        {
+            int remainder = minute % 60;
+            int complement = (60 - remainder) % 60;
+
+            if (remainderCount.find(complement) != remainderCount.end())
+            {
+                count += remainderCount[complement];
+                cout << minute << " " << complement << endl;
+            }
+            remainderCount[remainder]++;
+        }
+
+        return count;
+    }
+
+    int main()
+    {
+        int n;
+        cin >> n;
+        vector<int> minutes(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> minutes[i];
+        }
+        int result = countPairs(minutes);
+        cout << "Number of pairs: " << result << endl;
+        return 0;
+    }
+ 
